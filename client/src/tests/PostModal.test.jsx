65f850f5b4100e43e01e7fa5e5137b96423904d8
yukkigu <yukkigu@gmail.com>
2024-11-04 19:09:03 -0800

    removed unused imports

--- client/src/tests/PostModal.test.jsx
+++ client/src/tests/PostModal.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor, act } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import PostModal from "../components/PostModal";
 
 const renderPostModal = (isOpen = true) => {
