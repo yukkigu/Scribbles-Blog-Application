54fe47ff6b6b5f8e90d374fc338043899a04599a
yukkigu <yukkigu@gmail.com>
2024-11-06 00:28:21 -0800

    light/dark mode -- in progress

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import PostModal from "./PostModal";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 function NavBar(props) {
   const [createPostOpen, setCreatePostOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   function handleOpen() {
     setCreatePostOpen(true);
@@ -13,19 +16,30 @@ function NavBar(props) {
     setCreatePostOpen(false);
   }
 
+  function changeMode() {
+    if (darkMode) {
+      setDarkMode(false);
+    } else {
+      setDarkMode(true);
+    }
+  }
+
   return (
     <nav className="nav-bar">
       <ul>
         <a href="">Home</a>
         <a href="">About</a>
       </ul>
-      <button className="button new-post" aria-label="new" onClick={handleOpen}>
-        New Post
-      </button>
-      <PostModal
-        isOpen={createPostOpen}
-        onClose={handleClose}
-        submitPost={props.submitPost}></PostModal>
+      <div className="nav-container">
+        {darkMode ? <DarkModeIcon onClick={changeMode} /> : <LightModeIcon onClick={changeMode} />}
+        <button className="button new-post" aria-label="new" onClick={handleOpen}>
+          New Post
+        </button>
+        <PostModal
+          isOpen={createPostOpen}
+          onClose={handleClose}
+          submitPost={props.submitPost}></PostModal>
+      </div>
     </nav>
   );
 }
